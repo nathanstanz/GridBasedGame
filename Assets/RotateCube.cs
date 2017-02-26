3a1ecdc0f2e1fe3acc3d6e52e319d1a8659deeb3
NatePC <nathanstanz@gmail.com>
2017-02-25 23:43:26 -0800

    no message

---
+++ Assets/RotateCube.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space)) {
+            transform.Rotate(0, 0, -120 * Time.deltaTime);
+        }
+        
+	}
+}
